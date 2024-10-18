a9bfa24e760e2e3b6ba25142f01bea83a6ecdb45
alisson1010 <105652001+alisson1010@users.noreply.github.com>
2024-10-18 08:03:21 -0300

    lk

--- informaçõesGlobais.js
+++ informaçõesGlobais.js
@@ -1,5 +1,9 @@
 const url = https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
 
 async Function viasualizarIformaçõesGlobais() {
-    const res = 
+    const res = await  fetch (url)
+    const dados = await res.json()
+    console.log(dados)
 }
+
+viasualizarIformaçõesGlobais()
